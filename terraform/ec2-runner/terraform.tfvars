e27a6812a4a337994732d3af5f315c37835b594b
devops-bharat05 <bharatbhushan05@outlook.com>
2025-07-06 18:15:20 +0530

    ec2-runner-IaC added

---
+++ terraform/ec2-runner/terraform.tfvars
@@ -0,0 +1,5 @@
+aws_region     = "ap-south-1"
+aws_access_key = "your_access_key"
+aws_secret_key = "your_secret_key"
+key_name       = "your-keypair-name"
+
---
+++ terraform/ec2-runner/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "aws_access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_secret_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "key_name" {
+  default = "your-keypair-name"
+}
+
